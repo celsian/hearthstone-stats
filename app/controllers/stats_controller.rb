1067a446161f83ac378d27279f5b424dbe673df5
Celsian <celsian@gmail.com>
2013-11-16 19:19:33 -0800

    Removed un-necessary calls and added update/strong_params

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,19 +6,11 @@ class StatsController < ApplicationController
 
     if @user
       @arena = Arena.current_arena(@user)
-
       if @arena
         @arena_score = @arena.score_formatted
       end
-
-      @overall = @user.total_wins
-      @overall_winrate = @user.winrate
-
-      @winrate_turn = @user.winrate_turn
-      @series_stats = @user.series_stats
-      @as_class = @user.winrate_as_class
-      @against_class = @user.winrate_against_class
     end
+
   end
 
   def edit
@@ -26,10 +18,31 @@ class StatsController < ApplicationController
   end
 
   def update
+    stat = Stat.find(params[:id])
+    stat.update stat_params
 
+    if stat.save
+      redirect_to stat_path, flash: {success: "Your stat's were updated."}
+    else
+      render :edit
+    end
   end
 
   private
+
+  def stat_params
+    params.require(:stat).permit(:wins, :losses, :winrate_when_first_wins, :winrate_when_first_losses,
+      :winrate_when_second_wins, :winrate_when_second_losses, :total_series, :profitable_series,
+      :nine0, :ninex, :xthree, :zerothree, :wr_as_druid_wins, :wr_as_druid_losses, :wr_as_hunter_wins,
+      :wr_as_hunter_losses, :wr_as_mage_wins, :wr_as_mage_losses, :wr_as_paladin_wins, :wr_as_paladin_losses,
+      :wr_as_priest_wins, :wr_as_priest_losses, :wr_as_shaman_wins, :wr_as_shaman_losses, :wr_as_rogue_wins,
+      :wr_as_rogue_losses, :wr_as_warlock_wins, :wr_as_warlock_losses, :wr_as_warrior_wins, :wr_as_warrior_losses,
+      :wr_against_druid_wins, :wr_against_druid_losses, :wr_against_hunter_wins, :wr_against_hunter_losses,
+      :wr_against_mage_wins, :wr_against_mage_losses, :wr_against_paladin_wins, :wr_against_paladin_losses,
+      :wr_against_priest_wins, :wr_against_priest_losses, :wr_against_shaman_wins, :wr_against_shaman_losses,
+      :wr_against_rogue_wins, :wr_against_rogue_losses, :wr_against_warlock_wins, :wr_against_warlock_losses,
+      :wr_against_warrior_wins, :wr_against_warrior_losses)
+  end
 
   def require_same_username
     unless current_user && current_user.username == params[:username].downcase
