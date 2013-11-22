bda871ada5b447008977dea2dcc3670a7a206948
Celsian <celsian@gmail.com>
2013-11-21 18:08:19 -0800

    Added edit and update actions

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -24,6 +24,30 @@ class MatchesController < ApplicationController
     end
   end
 
+  def edit
+    @match = Match.find(params[:id])
+  end
+
+  def update
+    match = Match.find(params[:id])
+    enemy = match.enemy
+    first = match.first
+    win = match.win
+
+    match.update match_params
+
+    #Need to -= 1 from all the previous values so:
+    User.remove_match_stats(enemy, first, win, match, current_user)
+
+    if match.save
+      current_user.add_match_stats(match)
+
+      redirect_to arenas_path, flash: {success: "Match results successfully updated."}
+    else
+      render :new
+    end
+  end
+
   private
 
   def match_params
